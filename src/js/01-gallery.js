d71eda356fa14e3e220eadc7fe5d087a46cf66f1
KarolMalinowski0605 <126602802+KarolMalinowski0605@users.noreply.github.com>
2023-08-17 21:02:11 +0200

    hw-SimpleLightbox library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function creatingGallery() {
+  const addHtml = galleryItems.map(item =>
+    `<div class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </div>`
+  ).join('');
+
+  galleryContainer.innerHTML = addHtml;
+}
+
+creatingGallery();
+
+const lightbox = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
